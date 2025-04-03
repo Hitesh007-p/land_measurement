353e32ebe3fda1c8be59ba7c4ec93e1a2b8b9121
Hitesh patil <hiteshpatil4000@gmail.com>
2025-04-03 18:07:30 +0530

    first commit

---
+++ android/app/src/main/java/com/example/land_mesure/MainApplication.java
@@ -0,0 +1,13 @@
+package com.example.land_mesure;
+
+import android.app.Application;
+import android.content.Context;
+import androidx.annotation.NonNull;
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Initialize any global components here
+    }
+} 
